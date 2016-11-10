614ecb82f0480fd3c7cc1cb2b41d46476337cbf3
Rekha Manjukiran <rekha@manjukiran.com>
2016-11-10 22:35:55 +0000

    initial commit

---
+++ Deevee/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Deevee
+//
+//  Created by Manju Kiran on 10/11/2016.
+//  Copyright © 2016 RMK Associates. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Deevee/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Deevee
+//
+//  Created by Manju Kiran on 10/11/2016.
+//  Copyright © 2016 RMK Associates. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
